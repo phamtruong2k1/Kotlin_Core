5c39ccf8a3f2c1b701e2b2c8f316971523214ee6
Phạm Ánh Trường <61191253+phamtruong2k1@users.noreply.github.com>
2023-09-10 17:24:07 +0700

    push code

--- B2_For_Whie_Array/src/main/kotlin/Main.kt
+++ B2_For_Whie_Array/src/main/kotlin/Main.kt
@@ -1,7 +1,57 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+
+    val arr = arrayOf<Int>(
+        10, 11, 12, 13
+    )
+
+
+    for (i in 1..9 step 2) {
+        println(i)
+    }
+
+    for (i in 0 until 9 step 5) {
+        println(i)
+    }
+
+    for (i in 9 downTo  1 step 5) {
+        for (i in 0 until 9 step 5) {
+            println(i)
+            break
+        }
+    }
+
+    for (i in 9 downTo  1 step 5) {
+        for (i in 9 downTo  1 step 5) {
+            println(i)
+            if (i % 2 ==0 ){
+                continue
+            }
+            println(i)
+        }
+    }
+
+    //Duyệt tuần tự
+    for (item in arr) {
+        println(item)
+    }
+
+    //Duyệt tuần tự
+    arr.forEach { item->
+        println(item)
+    }
+
+    //Duyệt tuần tự có index
+    arr.forEachIndexed { index, item ->
+        println("$index : $item")
+    }
+
+    var n = 9
+    while (n >4) {
+        println(n)
+        n--
+    }
+
+    //Lưu ý: Duyệt tuần tự k dc thay dổi vị trí
 }
